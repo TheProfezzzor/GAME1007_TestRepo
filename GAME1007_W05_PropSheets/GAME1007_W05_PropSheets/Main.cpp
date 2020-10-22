189f91fa0405b419ddd9f8a6e13ca36a0a7d5108
TheProfezzzor <TheProfezzzor@users.noreply.github.com>
2020-10-22 13:51:32 -0400

    Added foo Function
    Added foo Function.

--- GAME1007_W05_PropSheets/GAME1007_W05_PropSheets/Main.cpp
+++ GAME1007_W05_PropSheets/GAME1007_W05_PropSheets/Main.cpp
@@ -2,9 +2,9 @@
 #include <sdl.h>
 using namespace std;
 
-void testFunction()
+void fooFunction()
 {
-	cout << "Test function invoked..." << endl;
+	cout << "Foo function invoked..." << endl;
 }
 
 int main(int argc, char* argv[])
@@ -40,7 +40,7 @@ int main(int argc, char* argv[])
 	SDL_SetRenderDrawColor(p_renderer, 255, 128, 64, 255); // 0-255 for each color channel
 	SDL_RenderClear(p_renderer); // 'paints over' window with draw color
 	SDL_RenderPresent(p_renderer); // Refreshes window
-	testFunction();
+	fooFunction();
 	system("pause"); // Just to pause before quit.
 	SDL_DestroyRenderer(p_renderer);
 	SDL_DestroyWindow(p_window);
